0303a2b91f6aacc5ebbcd4d3f4b66392be8aa612
zamanshahed <sumonshd@gmail.com>
2023-08-05 09:36:30 +0600

    code clean ups

--- app/page.js
+++ app/page.js
@@ -1,52 +1,11 @@
 "use client"
-import Image from 'next/image'
-import styled from "styled-components"
-import { useLocalStorage } from 'usehooks-ts';
-import { defaultTheme } from './themes/defaultTheme';
-import { darkTheme } from './themes/darkTheme';
-import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const [, setTheme] = useLocalStorage("theme", defaultTheme);
-  const { push } = useRouter()
-
 
   return (
-    <Container>
-      <PrimaryButton
-        variant="contained"
-        color="primary"
-        style={{ marginRight: 8 }}
-        onClick={() => push('/dashboard/home')}
-      >
-        dashboard
-      </PrimaryButton>
-      <PrimaryButton
-        variant="contained"
-        color="secondary"
-        onClick={() => push('/login')}
-      >
-        Login
-      </PrimaryButton>
-    </Container>
+    <div className="text-5xl font-medium bg-red-500">
+      Hey mom! i am doing nextjs 13 with redux and tailwindcss
+    </div>
   )
 }
 
-
-const Container = styled.div`
-  min-height: 100vh;
-  min-width: 100vw;
-  padding: 0px;
-  margin: 0px;
-  background-color: ${ props => props.theme.bodyColor};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-const PrimaryButton = styled.button`
-  width : 200px;
-  height: 50px;
-  background-color: white;
-  border-radius: 20px;
-
-`
